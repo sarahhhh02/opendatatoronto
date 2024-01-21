164d69dc9349acd3138ec8648eb28fafb2aeefa2
Sarah Lee <sarahhhh.lee@mail.utoronto.ca>
2024-01-21 00:37:25 -0500

    Update the scripts/01-download_data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -6,32 +6,33 @@
 # License: MIT
 # Any other information needed? [...UPDATE THIS...]
 
-
 #### Workspace setup ####
+install.packages("tidyverse")
+install.packages("dplyr")
+install.packages("janitor")
+install.packages("knitr")
 
 library(opendatatoronto)
 library(tidyverse)
 library(dplyr)
+library(janitor)
+library(knitr)
 
 #### Download data ####
 # get package
-package <- show_package("7d72bbbe-8adc-4b36-8ad1-5359f1c7a9cc")
-package
+Homicides_data <- 
+  list_package_resources("7d72bbbe-8adc-4b36-8ad1-5359f1c7a9cc") |>
+  filter(name == "Homicides - 4326.csv") |>
+  get_resource()
 
-# get all resources for this package
-resources <- list_package_resources("7d72bbbe-8adc-4b36-8ad1-5359f1c7a9cc")
-
-# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
-datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
-
-# load the first datastore resource as a sample
-data <- filter(datastore_resources, row_number()==1) %>% get_resource()
-data
-
+print(Homicides_data)
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(
+  x = Homicides_data, 
+  file = "inputs/data/Homicides_data.csv"
+  ) 
 
          
