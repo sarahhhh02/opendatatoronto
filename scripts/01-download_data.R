1cbdddf78a22172c7985bd3f905ea485fb3572d5
Sarah Lee <sarahhhh.lee@mail.utoronto.ca>
2024-01-20 23:40:29 -0500

    Update the scripts/01-download_data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,21 +1,32 @@
 #### Preamble ####
 # Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Author: Sarah Lee 
+# Date: 20 January 2024 
+# Contact: sarahhhh.lee@utoronto.ca [...UPDATE THIS...]
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
 # Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
+
 library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+library(dplyr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# get package
+package <- show_package("7d72bbbe-8adc-4b36-8ad1-5359f1c7a9cc")
+package
 
+# get all resources for this package
+resources <- list_package_resources("7d72bbbe-8adc-4b36-8ad1-5359f1c7a9cc")
+
+# identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
+datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
+
+# load the first datastore resource as a sample
+data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+data
 
 
 #### Save data ####
